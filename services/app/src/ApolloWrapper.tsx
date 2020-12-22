2108caccbeb588fff98f9eb286c4d2915d5bef10
Avi Moondra <avimoondra@gmail.com>
2020-12-22 13:39:01 -0500

    read auth from env (temp) (#35)

--- services/app/src/ApolloWrapper.tsx
+++ services/app/src/ApolloWrapper.tsx
@@ -49,7 +49,8 @@ const createApolloClient = () => {
               "X-Hasura-Role": "FILL_ME_IN",
             }
           : {
-              "x-hasura-admin-secret": "myadminsecretkey",
+              "x-hasura-admin-secret":
+                process.env.REACT_APP_BESPOKE_HASURA_ADMIN_SECRET,
             },
       },
     },
