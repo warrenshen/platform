654b619b01e2c4b904f8dc5a030fb90aa9271929
Warren Shen <warrenzshen@gmail.com>
2021-01-17 15:39:44 -0800

    [app] Render correct user role on Overview dashboard (#81)
    * Render correct user role on Overview dashboard
    
    * Run prettier on changes

--- services/app/src/pages/Home/index.tsx
+++ services/app/src/pages/Home/index.tsx
@@ -1,11 +1,21 @@
+import { CurrentUserContext, UserRole } from "contexts/CurrentUserContext";
 import useAppBarTitle from "hooks/useAppBarTitle";
+import { useContext } from "react";
 import { useTitle } from "react-use";
 
 function Home() {
   useTitle("Overview | Bespoke");
   useAppBarTitle("Overview");
 
-  return <div>Dashboard for customer</div>;
+  const { user } = useContext(CurrentUserContext);
+
+  return (
+    <div>
+      {`Dashboard for ${
+        user.role === UserRole.BankAdmin ? "BANK USER" : "CUSTOMER USER"
+      }`}
+    </div>
+  );
 }
 
 export default Home;
