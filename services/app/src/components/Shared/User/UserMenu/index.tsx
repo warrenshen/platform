99a4b56a1ba004b7d0dd9d5216decf5198c59882
Warren Shen <warrenzshen@gmail.com>
2021-01-17 18:12:17 -0800

    Render email of current user in UserMenu component (#84)

--- services/app/src/components/Shared/User/UserMenu/index.tsx
+++ services/app/src/components/Shared/User/UserMenu/index.tsx
@@ -3,17 +3,27 @@ import {
   NormalizedCacheObject,
   useApolloClient,
 } from "@apollo/client";
-import { IconButton, Menu, MenuItem } from "@material-ui/core";
+import { Box, IconButton, Menu, MenuItem } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
 import { CurrentUserContext } from "contexts/CurrentUserContext";
+import { UserFragment, useUserByIdQuery } from "generated/graphql";
+import { Maybe } from "graphql/jsutils/Maybe";
+import { routes } from "lib/routes";
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import { routes } from "lib/routes";
 
 function UserMenu() {
   const client = useApolloClient() as ApolloClient<NormalizedCacheObject>;
-  const { signOut } = useContext(CurrentUserContext);
+  const { user: currentUser, signOut } = useContext(CurrentUserContext);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const { data } = useUserByIdQuery({
+    variables: {
+      id: currentUser.id,
+    },
+  });
+
+  const user: Maybe<UserFragment> = data?.users_by_pk;
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -25,9 +35,12 @@ function UserMenu() {
 
   return (
     <>
-      <IconButton onClick={handleClick}>
-        <AccountCircle></AccountCircle>
-      </IconButton>
+      <Box display="flex" flexDirection="row" alignItems="center">
+        <Box>{user?.email}</Box>
+        <IconButton onClick={handleClick}>
+          <AccountCircle></AccountCircle>
+        </IconButton>
+      </Box>
       <Menu
         anchorEl={anchorEl}
         keepMounted
