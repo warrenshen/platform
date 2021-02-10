5af94dcd4cec0ec36c71a34cf1f18cac1bc92a98
dasha <dasha@interacta.io>
2021-02-11 00:50:28 +0500

    Reset password page (#273)
    * Reset password page
    
    * Reset password page | Make inputs required
    
    * Reset password page | Successful sumbit case
    
    * Reset password page | prettier

--- services/app/src/pages/Anonymous/ResetPassword/index.tsx
+++ services/app/src/pages/Anonymous/ResetPassword/index.tsx
@@ -1,17 +1,211 @@
+import {
+  Box,
+  Button,
+  createStyles,
+  FormHelperText,
+  makeStyles,
+  TextField,
+  Typography,
+  Theme,
+} from "@material-ui/core";
+import { CurrentUserContext } from "contexts/CurrentUserContext";
+import { routes } from "lib/routes";
+import { authenticatedApi, authRoutes } from "lib/api";
+import { useContext, useEffect, useState, useMemo } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { useTitle } from "react-use";
 interface Props {
   location: any;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      width: "100%",
+      height: "100%",
+      float: "left",
+      margin: 0,
+      padding: 0,
+      backgroundSize: "cover",
+      overflow: "hidden",
+      backgroundImage: `url(${process.env.PUBLIC_URL}/signInBackground.jpg)`,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    formContainer: {
+      background: "white",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      width: 400,
+      padding: theme.spacing(3),
+      paddingTop: theme.spacing(2),
+    },
+    imageBox: {
+      display: "flex",
+      justifyContent: "center",
+    },
+    formInput: {
+      display: "flex",
+      margin: theme.spacing(2),
+    },
+    error: {
+      marginTop: 0,
+      marginLeft: theme.spacing(2),
+      marginRight: theme.spacing(2),
+    },
+    form: {
+      height: "150px",
+    },
+    formHeader: {
+      fontSize: "18px",
+      margin: "auto",
+      padding: "12px 0",
+    },
+    submitButton: {
+      marginTop: theme.spacing(2),
+      width: 120,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  })
+);
+
 function ResetPassword(props: Props) {
+  const classes = useStyles();
+  useTitle("Reset Password | Bespoke");
+
   const linkVal = props.location.state?.link_val;
+
+  const history = useHistory();
+  const { state }: any = useLocation();
+  const { isSignedIn } = useContext(CurrentUserContext);
+  const [error, setError] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [confirmationPassword, setConfirmationPassword] = useState("");
+
+  const notEmptyPasswords = useMemo<boolean>(
+    () => !!password && !!confirmationPassword,
+    [password, confirmationPassword]
+  );
+
+  const canSubmit = useMemo<boolean>(
+    () => notEmptyPasswords && password === confirmationPassword,
+    [notEmptyPasswords, password, confirmationPassword]
+  );
+
+  const passwordsMatch = useMemo<boolean>(
+    () => notEmptyPasswords && confirmationPassword !== password,
+    [notEmptyPasswords, confirmationPassword, password]
+  );
+
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push(state?.from || routes.root);
+    }
+  }, [isSignedIn, history, state?.from]);
+
+  const onFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const params = {
+        password,
+        link_val: linkVal,
+      };
+      const response = await authenticatedApi.post(
+        authRoutes.resetPassword,
+        params
+      );
+      if (response.data?.status === "ERROR") {
+        alert(response.data?.msg);
+      } else {
+        setSuccess(true);
+      }
+    } catch (err) {
+      setError(
+        "Error encountered while updating the password. Please try again!"
+      );
+    }
+  };
 
   return (
     <>
-      <div>The reset password page</div>
-      <div>
-        You need to pass this link value to the /auth/reset-password POST
-        request: {linkVal}
-      </div>
+      <Box className={classes.container}>
+        <form onSubmit={onFormSubmit} className={classes.formContainer}>
+          <Box className={classes.imageBox}>
+            <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Logo" />
+          </Box>
+          {!success ? (
+            <>
+              <Typography className={classes.formHeader}>
+                Reset password
+              </Typography>
+              <div className={classes.form}>
+                <TextField
+                  data-cy="reset-password-input"
+                  type="password"
+                  label="New password"
+                  required
+                  className={classes.formInput}
+                  value={password}
+                  onChange={({ target: { value } }) => {
+                    error.length && setError("");
+                    setPassword(value);
+                  }}
+                ></TextField>
+                <TextField
+                  data-cy="reset-password-input-confirmation"
+                  type="password"
+                  label="Confirm password"
+                  error={passwordsMatch}
+                  required
+                  className={classes.formInput}
+                  value={confirmationPassword}
+                  helperText={passwordsMatch ? "Passwords do not match" : ""}
+                  onChange={({ target: { value } }) => {
+                    error.length && setError("");
+                    setConfirmationPassword(value);
+                  }}
+                ></TextField>
+              </div>
+              {error && (
+                <FormHelperText className={classes.error} error>
+                  {error}
+                </FormHelperText>
+              )}
+              <Button
+                data-cy="submit-button"
+                type="submit"
+                className={classes.submitButton}
+                disabled={!canSubmit}
+                variant="contained"
+                color="primary"
+              >
+                Submit
+              </Button>
+            </>
+          ) : (
+            <>
+              <Typography className={classes.formInput}>
+                Password has been successfully updated!
+              </Typography>
+              <Button
+                data-cy="submit-button"
+                type="submit"
+                className={classes.submitButton}
+                variant="contained"
+                color="primary"
+                onClick={() => history.push(routes.signIn)}
+              >
+                Sign in
+              </Button>
+            </>
+          )}
+        </form>
+      </Box>
     </>
   );
 }
