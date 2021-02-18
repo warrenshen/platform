ef132a9b4951e9a7e5bf93c0e1e75cda4ee9dda3
Warren Shen <warrenzshen@gmail.com>
2021-02-18 10:29:53 -0800

    Remove approval requested loans from bank overview (#346)
    * Remove None payment method and add questions to guide user in Pay Off Loan(s) step one
    
    * Prettier
    
    * Remove approval requested loans from overview dashboard

--- services/app/src/pages/Bank/Overview/index.tsx
+++ services/app/src/pages/Bank/Overview/index.tsx
@@ -1,22 +1,12 @@
 import Page from "components/Shared/Page";
 import {
   LoanStatusEnum,
-  RequestStatusEnum,
   useLoansByStatusesForBankQuery,
 } from "generated/graphql";
 import { bankRoutes } from "lib/routes";
 import BankOverviewLoansTable from "pages/Bank/Overview/BankOverviewLoansTable";
 
 function BankOverviewPage() {
-  const {
-    data: approvalRequestedLoansData,
-    error: approvalRequestedLoansError,
-  } = useLoansByStatusesForBankQuery({
-    variables: {
-      statuses: [LoanStatusEnum.ApprovalRequested],
-    },
-  });
-
   const {
     data: maturingLoansData,
     error: maturingLoansError,
@@ -35,12 +25,6 @@ function BankOverviewPage() {
     },
   });
 
-  if (approvalRequestedLoansError) {
-    alert(
-      "Error querying approval requested loans. " + approvalRequestedLoansError
-    );
-  }
-
   if (maturingLoansError) {
     alert("Error querying maturing loans. " + maturingLoansError);
   }
@@ -49,20 +33,11 @@ function BankOverviewPage() {
     alert("Error querying past due loans. " + pastDueLoansError);
   }
 
-  const approvalRequestedLoans = approvalRequestedLoansData?.loans || [];
   const maturingLoans = maturingLoansData?.loans || [];
   const pastDueLoans = pastDueLoansData?.loans || [];
 
   return (
     <Page appBarTitle={"Overview"}>
-      <BankOverviewLoansTable
-        isMaturityVisible={false}
-        loans={approvalRequestedLoans}
-        tableName={"Loans Approval Requested"}
-        routeToTablePage={bankRoutes.loansApprovalRequested}
-        loansPastDue={false}
-        filterByStatus={RequestStatusEnum.ApprovalRequested}
-      ></BankOverviewLoansTable>
       <BankOverviewLoansTable
         isMaturityVisible
         loans={maturingLoans}
@@ -70,14 +45,14 @@ function BankOverviewPage() {
         routeToTablePage={bankRoutes.loansMaturing}
         loansPastDue={false}
         matureDays={14}
-      ></BankOverviewLoansTable>
+      />
       <BankOverviewLoansTable
         isMaturityVisible
         loans={pastDueLoans}
         tableName={"Loans Past Due"}
         routeToTablePage={bankRoutes.loansPastDue}
         loansPastDue={true}
-      ></BankOverviewLoansTable>
+      />
     </Page>
   );
 }
