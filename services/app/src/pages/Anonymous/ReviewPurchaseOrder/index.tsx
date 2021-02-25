e4aac762db114e978d330445249166b4f396d784
Warren Shen <warrenzshen@gmail.com>
2021-02-24 23:30:07 -0800

    Style review purchase order pages (#426)

--- services/app/src/pages/Anonymous/ReviewPurchaseOrder/index.tsx
+++ services/app/src/pages/Anonymous/ReviewPurchaseOrder/index.tsx
@@ -6,6 +6,7 @@ import {
   FormControlLabel,
   makeStyles,
   Theme,
+  Typography,
 } from "@material-ui/core";
 import DownloadThumbnail from "components/Shared/File/DownloadThumbnail";
 import {
@@ -14,6 +15,7 @@ import {
   usePurchaseOrderForReviewQuery,
 } from "generated/graphql";
 import { formatCurrency } from "lib/currency";
+import { formatDateString } from "lib/date";
 import { anonymousRoutes } from "lib/routes";
 import { useMemo, useState } from "react";
 import { useHistory } from "react-router-dom";
@@ -26,6 +28,24 @@ interface Props {
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    wrapper: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      width: "100vw",
+      height: "100vh",
+      overflow: "scroll",
+    },
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      width: "100%",
+      maxWidth: 500,
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8),
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2),
+    },
     dialogTitle: {
       borderBottom: "1px solid #c7c7c7",
     },
@@ -46,8 +66,8 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function ReviewPurchaseOrderPage(props: Props) {
   const classes = useStyles();
-  const location: any = props.location;
 
+  const location: any = props.location;
   const payload = location.state?.payload;
   const linkVal = location.state?.link_val;
   const purchaseOrderId = payload?.purchase_order_id;
@@ -98,93 +118,85 @@ function ReviewPurchaseOrderPage(props: Props) {
   const isDataReady = !isPurchaseOrderLoading;
 
   return isDataReady && purchaseOrder ? (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      width="100vw"
-      height="100vh"
-    >
-      <Box display="flex" flexDirection="column" width="400px">
+    <Box className={classes.wrapper}>
+      <Box className={classes.container}>
         <Box display="flex" flexDirection="column">
-          <Box>
-            <h2>{`${purchaseOrder.vendor?.name}, your approval is requested`}</h2>
-            <p>
+          <Typography variant="h5">{`${purchaseOrder.vendor?.name}, your approval is requested`}</Typography>
+          <Box mt={1}>
+            <Typography variant="body2">
               The purchase order listed below requires your approval before we
-              can process payment. Please review the information and press
-              either the approve or reject button. If you press reject, you will
-              be prompted to enter in a reason.
-            </p>
-          </Box>
-          <Box display="flex" flexDirection="row" m={1}>
-            <p className={classes.propertyLabel}>
-              <strong>Order Number:</strong>
-            </p>
-            <p>{purchaseOrder.order_number}</p>
-          </Box>
-          <Box display="flex" flexDirection="row" m={1}>
-            <p className={classes.propertyLabel}>
-              <strong>Amount:</strong>
-            </p>
-            <p>{formatCurrency(purchaseOrder.amount)}</p>
-          </Box>
-          <Box display="flex" flexDirection="row" m={1}>
-            <p className={classes.propertyLabel}>
-              <strong>Order Date:</strong>
-            </p>
-            <p>{purchaseOrder.order_date}</p>
-          </Box>
-          <Box display="flex" flexDirection="row" m={1}>
-            <p className={classes.propertyLabel}>
-              <strong>Debtor:</strong>
-            </p>
-            <p>{purchaseOrder.company?.name}</p>
+              can process payment. Please review the info and press either
+              approve or reject. If you press reject, you will be prompted to
+              enter in a reason.
+            </Typography>
           </Box>
         </Box>
-        <Box flexDirection="column" flexGrow={1}>
-          <Box display="flex" flexDirection="row" m={1}>
-            <p className={classes.propertyLabel}>
-              <strong>Status</strong>
-            </p>
-            <p>{purchaseOrder.status}</p>
-          </Box>
-          <Box>
-            <DownloadThumbnail fileIds={purchaseOrderFileIds} />
-          </Box>
-          <Box>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  disabled={true}
-                  checked={!!purchaseOrder?.is_cannabis}
-                />
-              }
-              label={"Order includes cannabis or derivatives"}
-            />
-          </Box>
-          {purchaseOrder?.is_cannabis && (
-            <Box>
-              <DownloadThumbnail fileIds={purchaseOrderCannabisFileIds} />
-            </Box>
-          )}
+        <Box display="flex" flexDirection="column" mt={2}>
+          <Typography variant="subtitle2" color="textSecondary">
+            Order Number
+          </Typography>
+          <Typography variant={"body1"}>
+            {purchaseOrder.order_number}
+          </Typography>
         </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Button
-            disabled={false}
-            onClick={() => setIsRejectModalOpen(true)}
-            variant={"contained"}
-            color={"secondary"}
-          >
-            Reject
-          </Button>
-          <Button
-            disabled={false}
-            onClick={() => setIsApproveModalOpen(true)}
-            variant={"contained"}
-            color={"primary"}
-          >
-            Approve
-          </Button>
+        <Box display="flex" flexDirection="column" mt={2}>
+          <Typography variant="subtitle2" color="textSecondary">
+            Amount
+          </Typography>
+          <Typography variant={"body1"}>
+            {formatCurrency(purchaseOrder.amount)}
+          </Typography>
+        </Box>
+        <Box display="flex" flexDirection="column" mt={2}>
+          <Typography variant="subtitle2" color="textSecondary">
+            Order Date
+          </Typography>
+          <Typography variant={"body1"}>
+            {formatDateString(purchaseOrder.order_date)}
+          </Typography>
+        </Box>
+        <Box display="flex" flexDirection="column" mt={2}>
+          <Typography variant="subtitle2" color="textSecondary">
+            Delivery Date
+          </Typography>
+          <Typography variant={"body1"}>
+            {formatDateString(purchaseOrder.delivery_date)}
+          </Typography>
+        </Box>
+        <Box display="flex" flexDirection="column" mt={2}>
+          <Typography variant="subtitle2" color="textSecondary">
+            Debtor
+          </Typography>
+          <Typography variant={"body1"}>
+            {purchaseOrder.company?.name}
+          </Typography>
+        </Box>
+        <Box display="flex" flexDirection="column" mt={2}>
+          <Typography variant="subtitle2" color="textSecondary">
+            Purchase Order File
+          </Typography>
+          <DownloadThumbnail fileIds={purchaseOrderFileIds} />
+        </Box>
+        <Box mt={2}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                disabled={true}
+                checked={!!purchaseOrder?.is_cannabis}
+              />
+            }
+            label={"Order includes cannabis or derivatives"}
+          />
+        </Box>
+        {purchaseOrder?.is_cannabis && (
+          <Box display="flex" flexDirection="column" mt={2}>
+            <Typography variant="subtitle2" color="textSecondary">
+              Cannabis or Derivatives File(s)
+            </Typography>
+            <DownloadThumbnail fileIds={purchaseOrderCannabisFileIds} />
+          </Box>
+        )}
+        <Box display="flex" justifyContent="center" mt={4}>
           {isApproveModalOpen && (
             <ReviewPurchaseOrderApproveModal
               purchaseOrder={purchaseOrder}
@@ -209,6 +221,26 @@ function ReviewPurchaseOrderPage(props: Props) {
               }
             />
           )}
+          <Box mr={2}>
+            <Button
+              disabled={false}
+              onClick={() => setIsRejectModalOpen(true)}
+              variant={"contained"}
+              color={"secondary"}
+            >
+              Reject
+            </Button>
+          </Box>
+          <Box>
+            <Button
+              disabled={false}
+              onClick={() => setIsApproveModalOpen(true)}
+              variant={"contained"}
+              color={"primary"}
+            >
+              Approve
+            </Button>
+          </Box>
         </Box>
       </Box>
     </Box>
--- services/app/src/pages/Anonymous/ReviewPurchaseOrderComplete/index.tsx
+++ services/app/src/pages/Anonymous/ReviewPurchaseOrderComplete/index.tsx
@@ -1,22 +1,49 @@
-import { Box } from "@material-ui/core";
+import {
+  Box,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    wrapper: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      width: "100vw",
+      height: "100vh",
+      overflow: "scroll",
+    },
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      width: "100%",
+      maxWidth: 500,
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8),
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2),
+    },
+  })
+);
 
 function ReviewPurchaseOrderCompletePage() {
+  const classes = useStyles();
+
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      width="100vw"
-      height="100vh"
-    >
-      <Box display="flex" flexDirection="column" width="400px">
-        <Box>
-          <h2>Thank you!</h2>
-          <p>
-            Your decision to approve or reject the purchase order was saved
-            successfully. If we need any further information from you, we will
-            contact you directly. You may now close this page.
-          </p>
+    <Box className={classes.wrapper}>
+      <Box className={classes.container}>
+        <Box display="flex" flexDirection="column">
+          <Typography variant="h5">Thank you!</Typography>
+          <Box mt={1}>
+            <Typography variant="body2">
+              Your decision to approve or reject the purchase order was saved
+              successfully. If we need any further information from you, we will
+              contact you directly. You may now close this page.
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </Box>
