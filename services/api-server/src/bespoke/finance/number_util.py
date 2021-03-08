85b5c1c5e1bd9eb1ab37c8a6d8a2537c63d6ce01
Warren Shen <warrenzshen@gmail.com>
2021-03-08 09:35:02 -0800

    Use match.isclose abs_tol always (#545)

--- services/api-server/src/bespoke/finance/number_util.py
+++ services/api-server/src/bespoke/finance/number_util.py
@@ -6,11 +6,7 @@ def to_dollar_format(amount: float) -> str:
 	return '${:,.2f}'.format(amount)
 
 def float_eq(a: float, b: float) -> bool:
-	if b == 0:
-		# If comparison against 0, use absolute tolerance (vs relative tolerance).
-		return math.isclose(a, b, abs_tol=0.0001)
-	else:
-		return math.isclose(a, b, rel_tol=0.00001)
+	return math.isclose(a, b, abs_tol=0.0001)
 
 def float_lt(a: float, b: float) -> bool:
 	return False if float_eq(a, b) else a < b
