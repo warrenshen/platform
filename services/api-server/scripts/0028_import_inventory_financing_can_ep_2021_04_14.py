4b0efc860a6cf34fb6a11b1d8f9e6b769d3a6f2c
Warren Shen <warrenzshen@gmail.com>
2021-04-14 10:42:12 -0700

    Write script to import CAN and EP historical information (#869)

---
+++ services/api-server/scripts/0028_import_inventory_financing_can_ep_2021_04_14.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from os import path
+
+# Path hack before we try to import bespoke
+sys.path.append(path.realpath(path.join(path.dirname(__file__), "../src")))
+
+from bespoke.db import models
+
+from lib import advances, companies, loans, purchase_orders, repayments
+
+
+def main() -> None:
+	if not os.environ.get("DATABASE_URL"):
+		print("You must set 'DATABASE_URL' in the environment to use this script")
+		exit(1)
+
+	engine = models.create_engine()
+	session_maker = models.new_sessionmaker(engine)
+
+	payor_vendors_path = 'scripts/data/payor_vendors_2021_04_12.xlsx'
+	with models.session_scope(session_maker) as session:
+		companies.load_into_db_from_excel(session, payor_vendors_path)
+
+	purchase_orders_path = 'scripts/data/can_ep_purchase_orders_2021_04_14.xlsx'
+	with models.session_scope(session_maker) as session:
+		purchase_orders.load_into_db_from_excel(session, purchase_orders_path)
+
+	loans_path = 'scripts/data/can_ep_loans_2021_04_14.xlsx'
+	with models.session_scope(session_maker) as session:
+		loans.load_into_db_from_excel(session, loans_path)
+
+	advances_path = 'scripts/data/can_ep_advances_2021_04_14.xlsx'
+	with models.session_scope(session_maker) as session:
+		advances.load_into_db_from_excel(session, advances_path)
+
+	repayments_path = 'scripts/data/can_ep_repayments_2021_04_14.xlsx'
+	with models.session_scope(session_maker) as session:
+		repayments.load_into_db_from_excel(session, repayments_path)
+
+if __name__ == "__main__":
+	main()
