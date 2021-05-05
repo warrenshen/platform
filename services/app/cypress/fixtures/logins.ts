5330c0363df02d0602aac83c39523283249843dc
Warren Shen <warrenzshen@gmail.com>
2021-05-05 11:39:51 -0700

    Set up 1st cypress tests that pass in local env: sign in (#1034)

--- services/app/cypress/fixtures/logins.ts
+++ services/app/cypress/fixtures/logins.ts
@@ -1,8 +1,6 @@
-const logins = {
-  bankAdmin: "admin@bespoke.com",
-  customerAdmin: "admin@customer.com",
+export const users = {
+  bankAdmin: "jira+bank@bespokefinancial.com",
+  customerAdmin: "jira+customer@bespokefinancial.com",
 };
 
 export const password = "password123";
-
-export default logins;
--- services/app/cypress/integration/sign_in.spec.ts
+++ services/app/cypress/integration/sign_in.spec.ts
@@ -1,13 +1,25 @@
-import logins, { password } from "../fixtures/logins";
+import { password, users } from "../fixtures/logins";
 
-describe("Sign in", () => {
+describe("Sign in: bank user", () => {
   it("can sign in with email/password", () => {
     cy.visit("/");
 
-    cy.dataCySelector("sign-in-input-email", "input").type(logins.bankAdmin);
+    cy.dataCySelector("sign-in-input-email", "input").type(users.bankAdmin);
     cy.dataCySelector("sign-in-input-password", "input").type(password);
     cy.dataCy("sign-in-button").click();
 
-    cy.get("h6").should("contain", "Overview");
+    cy.url().should('include', 'overview');
+  });
+});
+
+describe("Sign in: customer user", () => {
+  it("can sign in with email/password", () => {
+    cy.visit("/");
+
+    cy.dataCySelector("sign-in-input-email", "input").type(users.customerAdmin);
+    cy.dataCySelector("sign-in-input-password", "input").type(password);
+    cy.dataCy("sign-in-button").click();
+
+    cy.url().should('include', 'overview');
   });
 });
