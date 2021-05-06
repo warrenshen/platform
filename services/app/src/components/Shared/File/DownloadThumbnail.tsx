ffd31ca885e479413a9520d201ab2adc87006a6e
Warren Shen <warrenzshen@gmail.com>
2021-05-06 00:09:59 -0700

    Improve UX of DownloadThumbnail component: wait until new files are loaded before update state (#1048)

--- services/app/src/components/Shared/File/DownloadThumbnail.tsx
+++ services/app/src/components/Shared/File/DownloadThumbnail.tsx
@@ -108,7 +108,6 @@ export default function DownloadThumbnail({
         }
       }
     };
-    setFilesWithSignedUrls([]);
     getFilesWithSignedUrls();
   }, [fileIds, fileType, setFilesWithSignedUrls]);
 
