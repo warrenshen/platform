45905988a24ad9c0f220573c9e08516ecc74e44f
David L <david@sweatequity.vc>
2021-05-26 15:01:34 -0700

    Upload and install all data from Metrc (#1170)

--- services/api-server/src/bespoke/metrc/transfers_util.py
+++ services/api-server/src/bespoke/metrc/transfers_util.py
@@ -2,7 +2,9 @@ import datetime
 import json
 import logging
 from datetime import timedelta
-from typing import Any, Dict, List, Tuple, cast
+from dateutil import parser
+from sqlalchemy.orm.session import Session
+from typing import Any, Callable, Dict, List, Tuple, cast
 
 from bespoke import errors
 from bespoke.date import date_util
@@ -12,6 +14,23 @@ from bespoke.metrc.metrc_common_util import CompanyInfo
 from dateutil import parser
 from sqlalchemy.orm.session import Session
 
+class LabTest(object):
+
+	def __init__(self, lab_test_json: Dict) -> None:
+		self._lab_test_results = lab_test_json # its an array that comes from the API
+
+	def get_results_array(self) -> Dict:
+		return self._lab_test_results
+
+	def get_status(self) -> str:
+		if not self._lab_test_results:
+			return 'unknown'
+
+		for result in self._lab_test_results:
+			if not result['TestPassed']:
+				return 'failed'
+
+		return 'passed'
 
 class TransferPackages(object):
 
@@ -24,20 +43,23 @@ class TransferPackages(object):
 	def get_package_ids(self) -> List[str]:
 		return [t['PackageId'] for t in self._packages]
 
-	def get_package_models(self, transfer_id: str) -> List[models.MetrcPackage]:
+	def get_package_models(self, transfer_id: str, lab_tests: List[LabTest]) -> List[models.MetrcPackage]:
 		metrc_packages = []
-		for package in self._packages:
+		for i in range(len(self._packages)):
+			package = self._packages[i]
+
 			p = models.MetrcPackage()
-			p.package_id = package['PackageId']
+			p.package_id = '{}'.format(package['PackageId'])
 			p.transfer_id = cast(Any, transfer_id)
-			p.delivery_id = package['DeliveryId']
+			p.delivery_id = '{}'.format(package['DeliveryId'])
 			p.label = package['PackageLabel']
 			p.type = package['PackageType']
 			p.product_name = package['ProductName']
 			p.package_payload = package
-			# TODO(dlluncor): Fill in lab results information
-			p.lab_results_payload = {}
-			p.lab_results_status = ''
+			p.lab_results_payload = {
+				'lab_results': lab_tests[i].get_results_array()
+			}
+			p.lab_results_status = lab_tests[i].get_status()
 			metrc_packages.append(p)
 
 		return metrc_packages
@@ -80,7 +102,7 @@ class Transfers(object):
 			tr.company_id = cast(Any, company_id)
 			tr.license_id = cast(Any, license_id)
 			tr.created_date = parser.parse(t['CreatedDateTime']).date()
-			tr.delivery_id = t['DeliveryId']
+			tr.delivery_id = '{}'.format(t['DeliveryId'])
 			tr.manifest_number = t['ManifestNumber']
 			tr.transfer_payload = t
 			metrc_transfers.append(tr)
@@ -128,20 +150,66 @@ def populate_transfers_table(cur_date: datetime.date, company_info: CompanyInfo,
 			company_id=company_info.company_id,
 			license_id=license['license_id']
 		)
-		for metrc_transfer in metrc_transfers:
-			session.add(metrc_transfer)
-			session.flush()
-			transfer_id = metrc_transfer.id
 
+		all_metrc_packages = []
+		package_id_to_metrc_transfer = {}
+
+		for metrc_transfer in metrc_transfers:
+			logging.info('Downloading data for metrc transfer delivery_id={}'.format(metrc_transfer.delivery_id))
 			delivery_id = metrc_transfer.delivery_id
 			resp = rest.get(f'/transfers/v1/delivery/{delivery_id}/packages')
 			t_packages_json = json.loads(resp.content)
 
-			metrc_packages = TransferPackages(delivery_id, t_packages_json).get_package_models(
-				transfer_id=transfer_id
+			packages = TransferPackages(delivery_id, t_packages_json)
+			package_ids = packages.get_package_ids()
+
+			lab_tests = []
+			for package_id in package_ids:
+				resp = rest.get(f'/labtests/v1/results?packageId={package_id}')
+				lab_test_json = json.loads(resp.content)
+				#lab_test_json = [] # Use an empty list if you want to speed things up
+				lab_tests.append(LabTest(lab_test_json))
+
+			metrc_packages = packages.get_package_models(
+				transfer_id=None,
+				lab_tests=lab_tests
 			)
 			for metrc_package in metrc_packages:
-				session.add(metrc_package)
+				package_id_to_metrc_transfer[metrc_package.package_id] = metrc_transfer
+				all_metrc_packages.append(metrc_package)
+
+		# Delete packages before writing them
+		package_ids = [pkg.package_id for pkg in all_metrc_packages]
+		prev_metrc_packages = session.query(models.MetrcPackage).filter(
+			models.MetrcPackage.package_id.in_(package_ids)
+		)
+		for prev_metrc_package in prev_metrc_packages:
+			cast(Callable, session.delete)(prev_metrc_package)
+
+		session.flush()
+
+		# Delete previous transfers which might be in the DB and overlap
+		# with the delivery_ids that we are about to write to the DB
+		delivery_ids = [tr.delivery_id for tr in metrc_transfers]
+		prev_metrc_transfers = session.query(models.MetrcTransfer).filter(
+			models.MetrcTransfer.delivery_id.in_(delivery_ids)
+		)
+		for prev_metrc_transfer in prev_metrc_transfers:
+			cast(Callable, session.delete)(prev_metrc_transfer)
+
+		session.flush()
+
+		# Write the transfers and write the packages
+		for metrc_transfer in metrc_transfers:
+			session.add(metrc_transfer)
+
+		session.flush()
+
+		for metrc_package in all_metrc_packages:
+			cur_metrc_package = package_id_to_metrc_transfer[metrc_package.package_id]
+			transfer_id = cur_metrc_package.id
+			metrc_package.transfer_id = transfer_id
+			session.add(metrc_package)
 
 	return True, None
 
