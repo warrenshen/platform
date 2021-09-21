4d5b108092bce6ee2c354ac8c4134eb35870651a
jr-bespoke <90631098+jr-bespoke@users.noreply.github.com>
2021-09-21 19:37:56 -0400

    Summation Row Added to Overview Table (#1705)
    * Wrapped up formatting on summation row.
    
    * Warrens feedback on formatting and using a previously unknown to me shared function.
    
    Co-authored-by: work <work@JRs-MacBook-Pro.local>

--- services/app/src/components/BankFinancialSummaries/BankFinancialSummariesDataGrid.tsx
+++ services/app/src/components/BankFinancialSummaries/BankFinancialSummariesDataGrid.tsx
@@ -8,9 +8,36 @@ import {
 import { ProductTypeToLabel } from "lib/enum";
 import { ColumnWidths } from "lib/tables";
 import { useMemo } from "react";
+import CSS from "csstype";
+import { formatCurrency } from "lib/currency";
 
 interface Props {
   bankFinancialSummaries: BankFinancialSummaryFragment[];
+}
+
+function DefineCellStyle(col: Number) {
+  const firstCellWidth = "160px";
+  const cellStyle: CSS.Properties = {
+    textAlign: col === 1 ? "left" : "right",
+    borderBottom: "rgb(224, 224, 224) 1px solid",
+    borderCollapse: "collapse",
+    height: "50px",
+    lineHeight: "20px",
+    paddingTop: "1px",
+    paddingBottom: "1px",
+    paddingLeft: col === 1 ? "18px" : "14px",
+    paddingRight: col === 7 ? "18px" : "14px",
+    fontFamily:
+      'Roboto, RobotoFallback, "Noto Kufi Arabic", Helvetica, Arial, sans-serif',
+    fontSize: "14px",
+    verticalAlign: "middle",
+    width: col === 1 ? firstCellWidth : "auto",
+    maxWidth: col === 1 ? firstCellWidth : "auto",
+    minWidth: col === 1 ? firstCellWidth : "auto",
+    fontWeight: "bold",
+  };
+
+  return cellStyle;
 }
 
 export default function BankFinancialSummariesDataGrid({
@@ -85,8 +112,93 @@ export default function BankFinancialSummariesDataGrid({
     ],
     []
   );
+  const tableStyle: CSS.Properties = {
+    background: "rgb(255,255,255)",
+    borderSpacing: 0,
+    borderCollapse: "collapse",
+    tableLayout: "fixed",
+    width: "100%",
+  };
+  const rowStyle: CSS.Properties = {
+    borderCollapse: "collapse",
+    borderSpacing: 0,
+  };
+
+  const summationRow =
+    rows.length === 0 ? (
+      <></>
+    ) : (
+      <table
+        style={tableStyle}
+        className="dx-datagrid-table dx-datagrid-table-fixed"
+        role="presentation"
+      >
+        <tbody role="presentation">
+          <tr
+            style={rowStyle}
+            className="dx-row dx-data-row dx-row-lines"
+            role="row"
+            aria-selected="false"
+            aria-rowindex={1}
+          >
+            <td style={DefineCellStyle(1)} role="gridcell" aria-colindex={1}>
+              Total
+            </td>
+            <td style={DefineCellStyle(2)} role="gridcell" aria-colindex={2}>
+              {formatCurrency(
+                rows.reduce(
+                  (sum, current) => sum + current.total_outstanding_principal,
+                  0
+                )
+              )}
+            </td>
+            <td style={DefineCellStyle(3)} role="gridcell" aria-colindex={3}>
+              {formatCurrency(
+                rows.reduce(
+                  (sum, current) => sum + current.interest_accrued_today,
+                  0
+                )
+              )}
+            </td>
+            <td style={DefineCellStyle(4)} role="gridcell" aria-colindex={4}>
+              {formatCurrency(
+                rows.reduce(
+                  (sum, current) => sum + current.total_outstanding_fees,
+                  0
+                )
+              )}
+            </td>
+            <td style={DefineCellStyle(5)} role="gridcell" aria-colindex={5}>
+              {formatCurrency(
+                rows.reduce(
+                  (sum, current) =>
+                    sum + current.total_principal_in_requested_state,
+                  0
+                )
+              )}
+            </td>
+            <td style={DefineCellStyle(6)} role="gridcell" aria-colindex={6}>
+              {formatCurrency(
+                rows.reduce((sum, current) => sum + current.available_limit, 0)
+              )}
+            </td>
+            <td style={DefineCellStyle(7)} role="gridcell" aria-colindex={7}>
+              {formatCurrency(
+                rows.reduce(
+                  (sum, current) => sum + current.adjusted_total_limit,
+                  0
+                )
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
 
   return (
-    <ControlledDataGrid isExcelExport dataSource={rows} columns={columns} />
+    <>
+      <ControlledDataGrid isExcelExport dataSource={rows} columns={columns} />
+      {summationRow}
+    </>
   );
 }
