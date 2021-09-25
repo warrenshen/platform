0eb35a382cade204406589b1e7c5bf5a4684044a
Warren Shen <warrenzshen@gmail.com>
2021-09-24 22:40:38 -0700

    Change how reset metrc transfers lab results status works to fetch transfer packages by transfer instead of by id (#1724)

--- services/api-server/scripts/backfill/0086_reset_metrc_transfers_lab_results_status.py
+++ services/api-server/scripts/backfill/0086_reset_metrc_transfers_lab_results_status.py
@@ -21,54 +21,6 @@ def main(is_test_run: bool = True) -> None:
 	session_maker = models.new_sessionmaker(engine)
 
 	current_page = 0
-	TRANSFER_PACKAGE_BATCH_SIZE = 50
-	is_done = False
-	metrc_transfer_package_index = 0
-
-	while not is_done:
-		with models.session_scope(session_maker) as session:
-			print(f'[Page {current_page + 1}] Resetting metrc transfer packages...')
-
-			try:
-				metrc_transfer_packages_batch = cast(
-					List[models.MetrcTransferPackage],
-					session.query(models.MetrcTransferPackage).order_by(
-						models.MetrcTransferPackage.id
-					).offset(
-						current_page * TRANSFER_PACKAGE_BATCH_SIZE
-					).limit(TRANSFER_PACKAGE_BATCH_SIZE).all())
-
-			except Exception as e:
-				print(e)
-				print('[WARNING] Caught SQL query exception, sleeping for 5 seconds before retrying...')
-				time.sleep(5)
-				continue
-
-			if len(metrc_transfer_packages_batch) <= 0:
-				is_done = True
-				continue
-			else:
-				for metrc_transfer_package in metrc_transfer_packages_batch:
-					metrc_transfer_package_index += 1
-
-					metrc_transfer_package_payload = metrc_transfer_package.package_payload
-					lab_testing_state = metrc_transfer_package_payload['LabTestingState']
-
-					if not lab_testing_state:
-						lab_results_status = UNKNOWN_LAB_STATUS
-					elif lab_testing_state == 'TestPassed':
-						lab_results_status = 'passed'
-					else:
-						lab_results_status = 'failed'
-
-					if metrc_transfer_package.lab_results_status != lab_results_status:
-						print(f'[{metrc_transfer_package_index}] Updating metrc transfer package lab results status from {metrc_transfer_package.lab_results_status} to {lab_results_status}...')
-						if not is_test_run:
-							metrc_transfer_package.lab_results_status = lab_results_status
-
-				current_page += 1
-
-	current_page = 0
 	TRANSFER_BATCH_SIZE = 10
 	is_done = False
 	metrc_transfer_index = 0
@@ -120,16 +72,34 @@ def main(is_test_run: bool = True) -> None:
 					else:
 						transfer_packages = transfer_row_id_to_transfer_packages[transfer_row_id]
 
+					metrc_transfer_package_index = 0
+					for transfer_package in transfer_packages:
+						metrc_transfer_package_index += 1
+						transfer_package_payload = transfer_package.package_payload
+						lab_testing_state = transfer_package_payload['LabTestingState']
+
+						if not lab_testing_state:
+							transfer_package_lab_results_status = UNKNOWN_LAB_STATUS
+						elif lab_testing_state == 'TestPassed':
+							transfer_package_lab_results_status = 'passed'
+						else:
+							transfer_package_lab_results_status = 'failed'
+
+						if transfer_package.lab_results_status != transfer_package_lab_results_status:
+							print(f'[{metrc_transfer_package_index}] Updating metrc transfer package lab results status from {transfer_package.lab_results_status} to {transfer_package_lab_results_status}...')
+							if not is_test_run:
+								transfer_package.lab_results_status = transfer_package_lab_results_status
+
 					lab_results = list(map(
 						lambda transfer_package: transfer_package.lab_results_status,
 						transfer_packages
 					))
-					lab_results_status = get_final_lab_status(lab_results)
+					transfer_lab_results_status = get_final_lab_status(lab_results)
 
-					if metrc_transfer.lab_results_status != lab_results_status:
-						print(f'[{metrc_transfer_index}] Updating metrc transfer lab results status from {metrc_transfer.lab_results_status} to {lab_results_status}...')
+					if metrc_transfer.lab_results_status != transfer_lab_results_status:
+						print(f'[{metrc_transfer_index}] Updating metrc transfer ({str(metrc_transfer.id)}) lab results status from {metrc_transfer.lab_results_status} to {transfer_lab_results_status}...')
 						if not is_test_run:
-							metrc_transfer.lab_results_status = lab_results_status
+							metrc_transfer.lab_results_status = transfer_lab_results_status
 
 				current_page += 1
 
