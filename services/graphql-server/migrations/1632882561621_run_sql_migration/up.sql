39187ed76ddc65e52e9f02d411f7939a68162ade
Warren Shen <warrenzshen@gmail.com>
2021-09-28 19:33:44 -0700

    Create two more table indices to speed up Metabase queries (#1740)

---
+++ services/graphql-server/migrations/1632882561621_run_sql_migration/up.sql
@@ -0,0 +1,1 @@
+CREATE INDEX IF NOT EXISTS metrc_transfers_company_id_created_date_key ON metrc_transfers (company_id, created_date);
---
+++ services/graphql-server/migrations/1632882597303_run_sql_migration/up.sql
@@ -0,0 +1,1 @@
+CREATE INDEX IF NOT EXISTS metrc_sales_transactions_receipt_row_id_key ON metrc_sales_transactions (receipt_row_id);
