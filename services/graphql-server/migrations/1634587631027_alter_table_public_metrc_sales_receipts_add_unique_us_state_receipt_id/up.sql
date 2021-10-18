c6b450bf962c2c06fdc8b3b8ae0ed33639cfaf8f
David L <david@sweatequity.vc>
2021-10-18 16:09:08 -0400

    Use a unique key with us_state in the receipt_number and receipt_id (#1833)

---
+++ services/graphql-server/migrations/1634587631027_alter_table_public_metrc_sales_receipts_add_unique_us_state_receipt_id/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."metrc_sales_receipts" drop constraint "metrc_sales_receipts_receipt_id_key";
+alter table "public"."metrc_sales_receipts" add constraint "metrc_sales_receipts_us_state_receipt_id_key" unique ("us_state", "receipt_id");
---
+++ services/graphql-server/migrations/1634587642328_alter_table_public_metrc_sales_receipts_add_unique_us_state_receipt_number/down.sql
@@ -0,0 +1,2 @@
+alter table "public"."metrc_sales_receipts" drop constraint "metrc_sales_receipts_us_state_receipt_number_key";
+alter table "public"."metrc_sales_receipts" add constraint "metrc_sales_receipts_receipt_number_key" unique ("receipt_number");
