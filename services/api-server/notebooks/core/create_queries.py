6a93e0d2a10e8b505a0df60c2df39405a81ae2da
David L <david@sweatequity.vc>
2021-11-01 15:40:09 -0400

    Query for UNKNOWN transfers (#1911)
    * Query for UNKNOWN transfers

--- services/api-server/notebooks/core/create_queries.py
+++ services/api-server/notebooks/core/create_queries.py
@@ -155,6 +155,60 @@ def create_company_outgoing_transfer_packages_query(company_identifier, start_da
             metrc_transfers.created_date desc
     """
 
+def create_company_unknown_transfer_packages_query(company_identifier, start_date):
+    return f"""
+        select
+            metrc_transfer_packages.id as package_row_id,
+            case
+                when company_deliveries.delivery_type = 'INCOMING_FROM_VENDOR' then 'INCOMING_FROM_VENDOR'
+                when company_deliveries.delivery_type = 'INCOMING_UNKNOWN' then 'INCOMING_FROM_VENDOR'
+                when company_deliveries.delivery_type = 'OUTGOING_UNKNOWN' then 'OUTGOING_TO_PAYOR'
+                when company_deliveries.delivery_type = 'OUTGOING_TO_PAYOR' then 'OUTGOING_TO_PAYOR'
+                else company_deliveries.delivery_type
+            end as delivery_type,
+            company_deliveries.license_number,
+            metrc_transfers.manifest_number,
+            metrc_transfers.created_date,
+            metrc_deliveries.received_datetime,
+            metrc_transfers.shipper_facility_license_number,
+            metrc_transfers.shipper_facility_name,
+            metrc_deliveries.recipient_facility_license_number,
+            metrc_deliveries.recipient_facility_name,
+            metrc_deliveries.shipment_type_name,
+            metrc_deliveries.shipment_transaction_type,
+            metrc_transfer_packages.package_id,
+            metrc_transfer_packages.package_label,
+            metrc_transfer_packages.type,
+            metrc_transfer_packages.package_payload.shipmentpackagestate as shipment_package_state,
+            metrc_transfer_packages.package_payload.istestingsample as is_testing_sample,
+            metrc_transfer_packages.package_payload.istradesample as is_trade_sample,
+            metrc_transfer_packages.product_category_name,
+            metrc_transfer_packages.product_name,
+            metrc_transfer_packages.lab_results_status as package_lab_results_status,
+            metrc_transfer_packages.shipper_wholesale_price,
+            metrc_transfer_packages.shipped_quantity,
+            metrc_transfer_packages.shipped_unit_of_measure,
+            metrc_transfer_packages.received_quantity,
+            metrc_transfer_packages.received_unit_of_measure,
+            metrc_transfer_packages.package_payload.itemunitweight as item_unit_weight,
+            metrc_transfer_packages.package_payload.itemunitweightunitofmeasurename as item_unit_weight_unit_of_measure_name
+        from
+            metrc_transfers
+            inner join company_deliveries on metrc_transfers.id = company_deliveries.transfer_row_id
+            inner join companies on company_deliveries.company_id = companies.id
+            inner join metrc_deliveries on metrc_transfers.id = metrc_deliveries.transfer_row_id
+            inner join metrc_transfer_packages on metrc_deliveries.id = metrc_transfer_packages.delivery_row_id
+        where
+            True
+            and companies.identifier = '{company_identifier}'
+            and (
+                company_deliveries.delivery_type = 'UNKNOWN'
+            )
+            and metrc_transfers.created_date >= '{start_date}'
+        order by
+            metrc_transfers.created_date desc
+    """
+
 def create_company_grouped_gmv_by_receipts_query(company_identifier, start_date, group_type):
     if group_type not in ['week', 'month']:
         return 'Invalid group type'
