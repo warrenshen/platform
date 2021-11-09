adcef8d70b8e0f7b5baa1960682e7de59abaf798
Warren Shen <warrenzshen@gmail.com>
2021-11-08 21:51:01 -0800

    Write backfill script 0094_delete_incorrect_loan_reports (#1969)

---
+++ services/api-server/scripts/backfill/0094_delete_incorrect_loan_reports.py
@@ -0,0 +1,77 @@
+"""
+DATABASE_URL=postgres+psycopg2://postgres:postgrespassword@localhost:5432/postgres python scripts/backfill/
+"""
+
+import os
+import sys
+import time
+from os import path
+from typing import Any,  Callable, Dict, List, cast
+
+# Path hack before we try to import bespoke
+sys.path.append(path.realpath(path.join(path.dirname(__file__), "../../src")))
+sys.path.append(path.realpath(path.join(path.dirname(__file__), "../")))
+
+from bespoke.db import models
+
+def main(is_test_run: bool = True) -> None:
+	engine = models.create_engine()
+	session_maker = models.new_sessionmaker(engine)
+
+	current_page = 0
+	BATCH_SIZE = 100
+	is_done = False
+
+	while not is_done:
+		with models.session_scope(session_maker) as session:
+			print(f'[Page {current_page + 1}] Trying to delete incorrect loan reports...')
+
+			try:
+				loan_reports = cast(
+					List[models.LoanReport],
+					session.query(models.LoanReport).order_by(
+						models.LoanReport.id.asc()
+					).offset(
+						current_page * BATCH_SIZE
+					).limit(BATCH_SIZE).all())
+
+				if len(loan_reports) <= 0:
+					is_done = True
+					continue
+
+				for loan_report in loan_reports:
+					loan = cast(
+						models.Loan,
+						session.query(models.Loan).filter(
+							models.Loan.loan_report_id == loan_report.id
+						).first())
+
+					if not loan:
+						print(f'Deleting loan report {str(loan_report.id)} since there is no corresponding loan for it...')
+						if not is_test_run:
+							session.delete(loan_report)
+
+				session.flush()
+
+			except Exception as e:
+				print(e)
+				print('[WARNING] Caught SQL query exception, sleeping for 5 seconds before retrying...')
+				time.sleep(5)
+				continue
+
+			current_page += 1
+
+if __name__ == "__main__":
+	if not os.environ.get("DATABASE_URL"):
+		print("You must set 'DATABASE_URL' in the environment to use this script")
+		exit(1)
+
+	is_test_run = True
+
+	if not os.environ.get("CONFIRM"):
+		print("This script CHANGES information in the database")
+		print("You must set 'CONFIRM=1' as an environment variable to actually perform database changes with this script")
+	else:
+		is_test_run = False
+
+	main(is_test_run)
