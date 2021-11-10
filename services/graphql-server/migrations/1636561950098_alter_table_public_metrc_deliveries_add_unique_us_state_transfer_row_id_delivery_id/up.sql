43e0eb52b61c160cb6effc120e2a7e3098e038ea
David L <david@sweatequity.vc>
2021-11-10 11:55:38 -0500

    Update the unique key on metrc_delivieries to include transfer_row_id (#1978)

---
+++ services/graphql-server/migrations/1636561950098_alter_table_public_metrc_deliveries_add_unique_us_state_transfer_row_id_delivery_id/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."metrc_deliveries" drop constraint "metrc_deliveries_us_state_delivery_id_key";
+alter table "public"."metrc_deliveries" add constraint "metrc_deliveries_us_state_transfer_row_id_delivery_id_key" unique ("us_state", "transfer_row_id", "delivery_id");
