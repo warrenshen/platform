e8c8fa133d9391173d529cb95910dc3ddfe708fa
Warren Shen <warrenzshen@gmail.com>
2021-12-30 11:48:20 -0800

    Improve disable bank holidays by considering case where observed NY is a different year than NY (#2155)

--- services/app/src/lib/holidays.ts
+++ services/app/src/lib/holidays.ts
@@ -8,33 +8,30 @@ export type Holidays = {
   };
 };
 
-function getObservedBankHolidays(
-  year: number
-): Record<string, Record<"date", Date>> {
-  const holidays = getBankHolidays(year) as Holidays;
-  return Object.keys(holidays).reduce((acc, holidayName) => {
-    const holiday = holidays[holidayName as Holiday];
-    if (isSaturday(holiday.date)) {
-      return {
-        ...acc,
-        [holidayName]: {
-          date: addDays(holiday.date, -1),
-        },
-      };
-    } else if (isSunday(holiday.date)) {
-      return {
-        ...acc,
-        [holidayName]: {
-          date: addDays(holiday.date, 1),
-        },
-      };
+function getBankHolidaysByYear(year: number) {
+  const holidayNameToHoliday = getBankHolidays(year) as Holidays;
+  return Object.keys(holidayNameToHoliday).map(
+    (holiday) => holidayNameToHoliday[holiday as Holiday].date
+  );
+}
+
+function getObservedBankHolidays(year: number) {
+  const currentYearHolidays = getBankHolidaysByYear(year);
+  const nextYearHolidays = getBankHolidaysByYear(year + 1);
+  const holidays = [...currentYearHolidays, ...nextYearHolidays];
+  const observedHolidays = holidays.map((bankHoliday) => {
+    if (isSaturday(bankHoliday)) {
+      return addDays(bankHoliday, -1);
+    } else if (isSunday(bankHoliday)) {
+      return addDays(bankHoliday, 1);
     } else {
-      return {
-        ...acc,
-        [holidayName]: holiday,
-      };
+      return null;
     }
-  }, {});
+  });
+  const validObservedHolidays = observedHolidays.filter(
+    (bankHoliday) => bankHoliday !== null
+  ) as Date[];
+  return [...holidays, ...validObservedHolidays];
 }
 
 export const bankHolidays = new Map();
@@ -43,11 +40,9 @@ export function addYearToBankHolidays(
   year: number,
   bankHolidays: Map<number, Set<string>>
 ): void {
-  const holidayNameToHoliday = getObservedBankHolidays(year);
+  const observedBankHolidays = getObservedBankHolidays(year);
   const holidays = new Set(
-    Object.keys(holidayNameToHoliday).map((holiday) =>
-      format(holidayNameToHoliday[holiday].date, "MM/dd/yyyy")
-    )
+    observedBankHolidays.map((date) => format(date, "MM/dd/yyyy"))
   );
   bankHolidays.set(year, holidays);
 }
