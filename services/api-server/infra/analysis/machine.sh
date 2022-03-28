5fc2ef5300cb83717449440b1f9d50f55b754b2e
jr-bespoke <90631098+jr-bespoke@users.noreply.github.com>
2022-03-28 16:38:03 -0400

    3.7 to 3.8 (#2368)

--- services/api-server/infra/analysis/machine.sh
+++ services/api-server/infra/analysis/machine.sh
@@ -5,15 +5,15 @@ sudo apt-get update
 sudo vi ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-# Setup Python 3.7
-sudo apt-get install -y python3.7
+# Setup Python 3.8
+sudo apt-get install -y python3.8
 sudo apt-get install -y python3-pip
 sudo apt-get install -y python3-dev
-python3.7 -m pip install pip-tools
+python3.8 -m pip install pip-tools
 
 # Install all requirements
 cd platform/services/api-server
-python3.7 -m pip install -r requirements.txt
+python3.8 -m pip install -r requirements.txt
 
 # Setup env variables and secrets
 vi .env
@@ -24,7 +24,7 @@ vi .env
 #
 # 
 # Use --use_cached_dataframes if doing a re-run where you are fixing a bug
-PYTHONPATH=src python3.7 scripts/analysis/compute_monthly_inventory.py --save_dataframes --num_threads=2 --num_processes=2 --write_to_db
+PYTHONPATH=src python3.8 scripts/analysis/compute_monthly_inventory.py --save_dataframes --num_threads=2 --num_processes=2 --write_to_db
 
 # Then to grab the files onto your local computer
 scp -i  ~/.ssh/bespoke_ec2_machine.pem ubuntu@54.218.119.190:/home/ubuntu/platform/services/api-server/out/many_companies_analysis_summary.xls ~/Downloads/many_companies_analysis_summary_12_09_2021.xls
