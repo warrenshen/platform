a4ddeb242ea45930e4f692cbe371203d737f9712
jr-bespoke <90631098+jr-bespoke@users.noreply.github.com>
2022-08-31 13:29:26 -0400

    Include Hotjar Script in Every Page (#2816)
    * init
    
    * init
    
    * saves purchase orders with new status
    
    * removed tests for now
    
    * updating styling of checkbox
    
    * added icons
    
    * cleanup
    
    * writing unit tests
    
    * accidental file across branches
    
    * set up hotjar script
    
    * anonymous function in react helmet caused site to crash, fixed by naming
    
    * small tweak to script type attribute
    
    * removed old hotjar
    
    * small typo tweak
    
    Co-authored-by: alan-bespoke <alan@bespokefinancial.com>

--- services/app/src/index.tsx
+++ services/app/src/index.tsx
@@ -4,6 +4,7 @@ import { CssBaseline } from "@material-ui/core";
 import * as Sentry from "@sentry/react";
 import { SnackbarProvider } from "material-ui-snackbar-provider";
 import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
 
 import App from "./App";
 import CustomSnackbar from "./components/Shared/Snackbar/CustomSnackbar";
@@ -18,6 +19,20 @@ Sentry.init({
 
 ReactDOM.render(
   <CurrentUserProvider>
+    <Helmet>
+      <script>
+        {`
+        (function hotjarSetup(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:2529958,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}
+      </script>
+    </Helmet>
     <ApolloWrapper>
       <SnackbarProvider SnackbarComponent={CustomSnackbar}>
         <CssBaseline>
