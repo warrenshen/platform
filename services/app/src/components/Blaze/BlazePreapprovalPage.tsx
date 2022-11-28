ee5c10110b21e20d79b68afb4dfc6b4d51afab78
Warren Shen <warrenzshen@gmail.com>
2022-11-28 12:39:55 -0800

    Turn on Blaze preapprovals - remove feature flag (#3030)

--- services/app/src/components/Blaze/BlazePreapprovalPage.tsx
+++ services/app/src/components/Blaze/BlazePreapprovalPage.tsx
@@ -1,8 +1,6 @@
 import { Box, Theme, createStyles, makeStyles } from "@material-ui/core";
-import BlazeComingSoonCard from "components/Blaze/BlazeComingSoonCard";
 import BlazePreapprovalCard from "components/Blaze/BlazePreapprovalCard";
 import { BlazePreapprovalFragment } from "generated/graphql";
-import { todayAsDateStringServer } from "lib/date";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -40,17 +38,14 @@ export default function BlazePreapprovalPage({
   blazePreapproval,
 }: Props) {
   const classes = useStyles();
-  const today = todayAsDateStringServer();
 
   return (
     <Box className={classes.wrapper}>
       <Box className={classes.container}>
         {isAuthenticateBlazeUserLoading ? (
           <div>Loading...</div>
-        ) : today > "2022-12-31" ? (
-          <BlazePreapprovalCard blazePreapproval={blazePreapproval} />
         ) : (
-          <BlazeComingSoonCard />
+          <BlazePreapprovalCard blazePreapproval={blazePreapproval} />
         )}
       </Box>
     </Box>
