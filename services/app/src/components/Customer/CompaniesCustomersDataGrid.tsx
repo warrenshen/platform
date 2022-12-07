28529a13a83c2c70d4a5419c8f7c090ba845f549
gracebespoke <106086938+gracebespoke@users.noreply.github.com>
2022-12-07 11:48:37 -0500

    Full Customers Data Grid (#3067)
    * fixing customers
    
    * fixing customers

--- services/app/src/components/Customer/CompaniesCustomersDataGrid.tsx
+++ services/app/src/components/Customer/CompaniesCustomersDataGrid.tsx
@@ -1,14 +1,42 @@
 import ClickableDataGridCell from "components/Shared/DataGrid/ClickableDataGridCell";
 import ControlledDataGrid from "components/Shared/DataGrid/ControlledDataGrid";
 import { CustomersWithMetadataFragment } from "generated/graphql";
-import { ProductTypeEnum, ProductTypeToLabel } from "lib/enum";
-import { CurrencyPrecision } from "lib/number";
+import { parseDateStringServer } from "lib/date";
+import {
+  CustomerSurveillanceCategoryEnum,
+  DebtFacilityCompanyStatusEnum,
+  ProductTypeEnum,
+  ProductTypeToLabel,
+} from "lib/enum";
+import { CurrencyPrecision, PercentPrecision } from "lib/number";
 import { BankCompanyRouteEnum, getBankCompanyRoute } from "lib/routes";
 import { ColumnWidths, formatRowModel } from "lib/tables";
 import { useMemo } from "react";
 
 interface Props {
   customers: CustomersWithMetadataFragment[];
+}
+
+function getBorrowingBaseDate(company: CustomersWithMetadataFragment) {
+  const borrowingBaseDate = !!company?.ebba_applications
+    ? company.ebba_applications.filter(
+        ({ category }) =>
+          category === CustomerSurveillanceCategoryEnum.BorrowingBase
+      )[0]?.application_date
+    : null;
+
+  return !!borrowingBaseDate ? parseDateStringServer(borrowingBaseDate) : null;
+}
+
+function getApplicationDate(company: CustomersWithMetadataFragment) {
+  const applicationDate = !!company?.ebba_applications
+    ? company.ebba_applications.filter(
+        ({ category }) =>
+          category === CustomerSurveillanceCategoryEnum.FinancialReport
+      )[0]?.application_date
+    : null;
+
+  return !!applicationDate ? parseDateStringServer(applicationDate) : null;
 }
 
 function getRows(customers: CustomersWithMetadataFragment[]) {
@@ -19,16 +47,55 @@ function getRows(customers: CustomersWithMetadataFragment[]) {
         ?.adjusted_total_limit
         ? company.financial_summaries[0].adjusted_total_limit
         : null,
+      application_date: getApplicationDate(company),
+      borrowing_base_date: getBorrowingBaseDate(company),
       company_url: getBankCompanyRoute(
         company.id,
         BankCompanyRouteEnum.Overview
       ),
       cy_identifier: `customers-data-grid-view-customer-button-${company.identifier}`,
+      debt_facility_status: !!company?.debt_facility_status
+        ? (company.debt_facility_status as DebtFacilityCompanyStatusEnum)
+        : null,
+      holding_account_balance:
+        !!company?.financial_summaries[0]?.account_level_balance_payload.hasOwnProperty(
+          "credits_total"
+        )
+          ? company.financial_summaries[0].account_level_balance_payload[
+              "credits_total"
+            ]
+          : null,
+      outstanding_account_fees:
+        !!company?.financial_summaries?.[0]?.account_level_balance_payload.hasOwnProperty(
+          "fees_total"
+        )
+          ? company.financial_summaries[0].account_level_balance_payload[
+              "fees_total"
+            ]
+          : null,
       product_type: !!company?.financial_summaries?.[0]
         ? ProductTypeToLabel[
             company.financial_summaries[0].product_type as ProductTypeEnum
           ]
         : "None",
+      daily_interest_rate: !!company?.financial_summaries?.[0]
+        ?.daily_interest_rate
+        ? company.financial_summaries[0].daily_interest_rate
+        : null,
+      state: !!company?.state ? company.state : null,
+      surveillance_status: !!company?.most_recent_surveillance_result?.[0]
+        ?.surveillance_status
+        ? company.most_recent_surveillance_result[0].surveillance_status
+        : null,
+      total_outstanding_interest: !!company?.financial_summaries?.[0]
+        ? company.financial_summaries[0].total_outstanding_interest
+        : null,
+      total_outstanding_fees: !!company?.financial_summaries?.[0]
+        ? company.financial_summaries[0].total_outstanding_fees
+        : null,
+      total_outstanding_principal: !!company?.financial_summaries?.[0]
+        ? company.financial_summaries[0].total_outstanding_principal
+        : null,
     });
   });
 }
@@ -67,6 +134,79 @@ export default function CompaniesCustomersDataGrid({ customers }: Props) {
           precision: CurrencyPrecision,
         },
         caption: "Borrowing Limit",
+        width: ColumnWidths.Currency,
+        alignment: "right",
+      },
+      {
+        dataField: "application_date",
+        format: "shortDate",
+        caption: "Most Recent Financial Report Date",
+        minWidth: ColumnWidths.Date,
+        alignment: "right",
+      },
+      {
+        dataField: "borrowing_base_date",
+        format: "shortDate",
+        caption: "Most Recent Borrowing Base Date",
+        minWidth: ColumnWidths.Date,
+        alignment: "right",
+      },
+      {
+        dataField: "daily_interest_rate",
+        format: {
+          type: "percent",
+          precision: PercentPrecision,
+        },
+        caption: "Daily Interest Rate",
+        minWidth: ColumnWidths.Currency,
+        alignment: "right",
+      },
+      {
+        dataField: "total_outstanding_principal",
+        format: {
+          type: "currency",
+          precision: CurrencyPrecision,
+        },
+        caption: "Total Outstanding Principal",
+        width: ColumnWidths.Currency,
+        alignment: "right",
+      },
+      {
+        dataField: "total_outstanding_interest",
+        format: {
+          type: "currency",
+          precision: CurrencyPrecision,
+        },
+        caption: "Total Outstanding Interest",
+        width: ColumnWidths.Currency,
+        alignment: "right",
+      },
+      {
+        dataField: "total_outstanding_fees",
+        format: {
+          type: "currency",
+          precision: CurrencyPrecision,
+        },
+        caption: "Total Outstanding Late Fees",
+        width: ColumnWidths.Currency,
+      },
+      {
+        dataField: "outstanding_account_fees",
+        format: {
+          type: "currency",
+          precision: CurrencyPrecision,
+        },
+        caption: "Outstanding Account Fees",
+        width: ColumnWidths.Currency,
+        alignment: "right",
+      },
+      {
+        dataField: "holding_account_balance",
+        format: {
+          type: "currency",
+          precision: CurrencyPrecision,
+        },
+        caption: "Holding Account Balance",
         width: ColumnWidths.Currency,
         alignment: "right",
       },
