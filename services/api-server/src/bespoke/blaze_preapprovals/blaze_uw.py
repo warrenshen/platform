1b7251e5978a92c030a53dbf14090ffbbd36ecd0
vic24x7 <97047967+vic24x7@users.noreply.github.com>
2022-12-07 16:03:06 -0800

    clean up blaze ca approval nb (#3069)
    Co-authored-by: victoria <>

---
+++ services/api-server/src/bespoke/blaze_preapprovals/blaze_uw.py
@@ -0,0 +1,392 @@
+import numpy
+def create_blaze_gm_query(State:str
+) -> str:
+    return f"""with base as 
+(
+   select
+      *,
+      last_day(week_start) year_month,
+      retail_value / quantity_sold per_unit_price,
+      cogs / quantity_sold per_unit_cost,
+      sales.retail_value - sales.total_discount + sales.total_tax sales_post_tax_post_discount,
+      sales.retail_value - sales.total_discount sales_pre_tax_post_discount,
+      sales.retail_value sales_pre_tax_pre_discount,
+   from
+      blaze_data.sales sales
+      -- EXCLUDE SAMPLES,DISPLAY,PROMO and test
+   where
+      lower(product_category) not like '%sample%' 
+      and lower(product_category) not like '%display%' 
+      and lower(product_category) not like '%promo%' 
+      and lower(product_category) not like '%test%'
+      and state = "{State}"
+)
+,
+monthly_location_rev_and_cogs as 
+(
+   select
+      year_month,
+      shop_id shopid,
+      company_id companyid,
+      sum(sales_post_tax_post_discount) rev_post_tax_post_discount,
+      sum(sales_pre_tax_post_discount) rev_pre_tax_post_discount,
+      sum(sales_pre_tax_pre_discount) rev_pre_tax_pre_discount,
+      sum(cogs) cogs 
+   from
+      base
+      -- EXCLUDE low COGS items
+   where
+      cogs > 1 
+      -- EXCLUDE high cost, high price items
+      and per_unit_price <= 500 
+      and per_unit_cost <= 500 
+   group by
+      2,
+      3,
+      1 
+   order by
+      2,
+      3,
+      1 
+)
+,
+monthly_location_rev_and_cogs_and_profit as 
+(
+   select
+      year_month,
+      shopid,
+      companyid,
+      -- rev and cogs
+      rev_post_tax_post_discount,
+      rev_pre_tax_post_discount,
+      rev_pre_tax_pre_discount,
+      cogs,
+      -- profit
+      rev_post_tax_post_discount - cogs profit_post_tax_post_discount,
+      rev_pre_tax_post_discount - cogs profit_pre_tax_post_discount,
+      rev_pre_tax_pre_discount - cogs profit_pre_tax_pre_discount,
+   from
+      monthly_location_rev_and_cogs 
+)
+,
+monthly_location_rev_and_cogs_and_profit_and_margin as 
+(
+   select
+      year_month,
+      shopid,
+      companyid,
+      -- rev and cogs
+      rev_post_tax_post_discount,
+      rev_pre_tax_post_discount,
+      rev_pre_tax_pre_discount,
+      cogs,
+      -- profit
+      profit_post_tax_post_discount,
+      profit_pre_tax_post_discount,
+      profit_pre_tax_pre_discount,
+      -- margin
+      profit_post_tax_post_discount / nullif(rev_post_tax_post_discount, 0) margin_post_tax_post_discount,
+      profit_pre_tax_post_discount / nullif(rev_pre_tax_post_discount, 0) margin_pre_tax_post_discount,
+      profit_pre_tax_pre_discount / nullif(rev_pre_tax_pre_discount, 0) margin_pre_tax_pre_discount 
+   from
+      monthly_location_rev_and_cogs_and_profit 
+)
+,
+monthly_gm_analysis as 
+(
+   select
+      year_month,
+      shopid,
+      companyid,
+      -- CHOOSE TO USE PRE TAX POST DISCOUNT
+      rev_pre_tax_post_discount,
+      lag(rev_pre_tax_post_discount) over (partition by shopid order by year_month) lag_rev_pre_tax_post_discount,
+      cogs,
+      profit_pre_tax_post_discount,
+      margin_pre_tax_post_discount,
+      count(distinct year_month) over (partition by shopid ) month_available,
+      sum(cogs) over (partition by shopid order by year_month rows between 2 preceding and current row ) sum_cogs_3m,
+      avg(margin_pre_tax_post_discount) over (partition by shopid order by year_month rows between 2 preceding and current row ) avg_margin_3m,
+      avg(margin_pre_tax_post_discount) over (partition by shopid order by year_month rows between 5 preceding and current row ) avg_margin_6m,
+      avg(margin_pre_tax_post_discount) over (partition by shopid order by year_month ) avg_margin_all 
+   from
+      monthly_location_rev_and_cogs_and_profit_and_margin 
+)
+select
+   *,
+   (
+      rev_pre_tax_post_discount - lag_rev_pre_tax_post_discount
+   )
+   / nullif(lag_rev_pre_tax_post_discount, 0) rev_change 
+from
+   monthly_gm_analysis
+                        """
+
+def create_blaze_gmv_change_query(State:str
+) -> str:
+    return f"""with base as
+(
+   select
+      shop_id,
+      company_id,
+      last_day(week_start) year_month,
+      sales.retail_value - sales.total_discount sales_pre_tax_post_discount
+   from
+      blaze_data.sales sales
+
+   where
+      lower(product_category) not like '%sample%'
+      and lower(product_category) not like '%display%'
+      and lower(product_category) not like '%promo%'
+      and lower(product_category) not like '%test%'
+      and state = "{State}"
+)
+,
+monthly_rev as
+(
+   select
+      year_month,
+      sum(sales_pre_tax_post_discount) rev_pre_tax_post_discount,
+      count(distinct shop_id) count_shop
+   from
+      base
+   group by
+1
+),
+
+monthly_rev_with_lag as
+(
+   select
+      year_month,
+      rev_pre_tax_post_discount,
+      count_shop,
+      lag(rev_pre_tax_post_discount) over (order by year_month) lag_rev_pre_tax_post_discount,
+      lag(count_shop) over (order by year_month) lag_count_shop
+   from
+      monthly_rev
+)
+select
+   *,
+    rev_pre_tax_post_discount / count_shop rev_per_shop,
+       lag_rev_pre_tax_post_discount / lag_count_shop lag_rev_per_shop,
+   (
+      rev_pre_tax_post_discount / count_shop - lag_rev_pre_tax_post_discount / lag_count_shop
+   )
+   / nullif(lag_rev_pre_tax_post_discount / lag_count_shop, 0) rev_change
+from
+   monthly_rev_with_lag
+order by year_month"""
+
+def create_blaze_inventory_date_query(State:str,uw_date:str
+) -> str:
+    return f"""select distinct date,
+last_day(date) 
+from blaze_data.inventory where state = "{State}"
+and last_day(Date) = "{uw_date}"
+"""
+
+def create_blaze_inventory_query(State:str,Date:tuple
+) -> str:
+    return f"""with base as (
+    select *,
+    DATE_DIFF(date,purchase_date,DAY) diff,
+    current_quantity * unit_cost total_value
+    from blaze_data.inventory
+    where  lower(product_category) not like '%sample%'
+    and lower(product_category) not like '%display%'
+    and lower(product_category) not like '%promo%'
+    and lower(product_category) not like '%test%'
+    -- EXCLUDE SMALL COGS
+    and unit_cost > 0.01
+    and current_quantity > 0
+    and current_quantity < 1000
+    and date in {Date}
+    and state = "{State}"
+),
+base_by_group as (
+select *,
+case when diff < 180 then 'fresh'
+     when diff >= 180 and diff < 270 then 'stale'
+    else  'bad' end as inventory_group
+from base
+),
+weekly_inv as (
+    select date,
+           shop_id          shopid,
+           company_id       companyid,
+           inventory_group,
+           sum(total_value) inventory
+    from base_by_group
+    group by 1, 2, 3,4
+)
+select
+shopid,
+companyid,
+inventory_group,
+avg(inventory) inventory,
+last_day('2022-10-31') year_month
+from weekly_inv
+group by 1,2,3"""
+
+def create_blaze_vc_query(State:str,Uw_date:str
+) -> str:
+    return f"""
+-- purcase order data
+with date as (
+    SELECT last_day(year_month) year_month_all
+FROM UNNEST(
+    GENERATE_DATE_ARRAY(DATE('2021-08-01'), CURRENT_DATE(), INTERVAL 1 month)
+) AS year_month
+),
+vendor as (
+    select distinct vendor_id from blaze_data.purchase_orders
+),
+shop as (
+    select distinct shop_id from blaze_data.purchase_orders
+    where state = "{State}"
+    and purchase_order_status = 'Closed'
+),
+all_data as (
+    with sub as (
+        select *
+        from date
+                 cross join vendor
+    )
+    select * from sub cross join shop
+),
+po_clean as (select * 
+from  (
+select DATE_DIFF(week_end, week_start, DAY) diff,* from blaze_data.purchase_orders
+             WHERE not (week_start = '2022-01-29' and week_end = '2022-02-05')
+and not (week_start = '2022-02-26' and week_end = '2022-03-05')
+and not (week_start = '2022-03-27' and week_end = '2022-04-03'))
+     where diff < 10)
+     ,
+po_data as (
+    select last_day(week_start) year_month,
+           shop_id,
+           vendor_id,
+           sum(total_cost)      total_amount
+
+    from po_clean
+    group by 1, 2, 3
+),
+
+vc_data as (
+    select all_data.year_month_all,
+           all_data.vendor_id,
+           all_data.shop_id,
+           COALESCE(po_data.total_amount, 0) amount,
+           sum(po_data.total_amount) over (partition by all_data.shop_id,all_data.vendor_id) facility_total,
+           sum(po_data.total_amount) over (partition by all_data.shop_id) all_vendor_total,
+           sum(po_data.total_amount) over (partition by all_data.shop_id,all_data.vendor_id order by all_data.year_month_all rows between 3 preceding and current row) last_4m_total
+           --lag( COALESCE(po_data.total_amount,0)) over (partition by all_data.vendor_id,all_data.shop_id order by year_month_all) lag_total_amount,
+
+    from all_data
+             left join po_data
+                       on all_data.year_month_all = po_data.year_month
+                           and all_data.vendor_id = po_data.vendor_id
+                           and all_data.shop_id = po_data.shop_id
+),
+
+vc_data_weight as (
+    select year_month_all,
+           vendor_id,
+           shop_id,
+           amount,
+           COALESCE(facility_total, 0)  vendor_total,
+           COALESCE(all_vendor_total, 0) all_vendor_total,
+           COALESCE(last_4m_total, 0) last_4m_total
+    from vc_data
+),
+vc_data_weight_clean as (
+    select
+    year_month_all,
+    vendor_id,
+    shop_id,
+    amount,
+    vendor_total,
+    all_vendor_total,
+    last_4m_total,
+    vendor_total / NULLIF(all_vendor_total, 0)  vendor_perc,
+    case when last_4m_total = 0 then '4m_inactive' else '4m_active' end as activity,
+    case
+    when  vendor_total/ NULLIF(all_vendor_total, 0) > 0.001 then 'significant'
+    else 'non-significant' end as significance
+    from vc_data_weight
+),
+vc_final as (
+    select
+    *,
+    case when activity = '4m_active' and significance = 'significant' then 'active'
+    else (case when significance = 'significant' then 'inactive' else 'exclude' end) end as measure
+    from vc_data_weight_clean
+    )
+
+
+select
+shop_id shopid,
+sum(vendor_perc) total_vc
+from vc_final
+where year_month_all = "{Uw_date}"
+and measure = 'inactive'
+group by 1
+"""
+
+
+def gmv_change_variance_point_mapping(x):
+    if x < -0.1:
+        return -5
+    elif x >= -0.1 and x < -0.05:
+        return -2.5
+    elif x >= -0.05 and x < 0:
+        return 0
+    elif x >= 0 and x < 0.05:
+        return 5
+    elif x >= 0.05 and x <= 0.5:
+        return 10
+    else:
+        return numpy.nan
+
+
+def get_gm_perc_scores(threshold, gm_3, gm_6, gm_all):
+    # 3m score
+    if gm_3 <= threshold[0][0]:
+        score_3m = -2
+    elif threshold[0][0] < gm_3 <= threshold[0][1]:
+        score_3m = 0
+    elif threshold[0][1] < gm_3 <= threshold[0][2]:
+        score_3m = 5
+    elif threshold[0][2] < gm_3 <= threshold[0][3]:
+        score_3m = 5
+    else:
+        score_3m = 0
+    # 6m score
+    if gm_6 <= threshold[1][0]:
+        score_6m = -2
+    elif threshold[1][0] < gm_6 <= threshold[1][1]:
+        score_6m = 0
+    elif threshold[1][1] < gm_6 <= threshold[1][2]:
+        score_6m = 5
+    elif threshold[1][2] < gm_6 <= threshold[1][3]:
+        score_6m = 5
+    else:
+        score_6m = 0
+    # all time score
+    if gm_all <= threshold[2][0]:
+        score_all = -2
+    elif threshold[2][0] < gm_all <= threshold[2][1]:
+        score_all = 0
+    elif threshold[2][1] < gm_all <= threshold[2][2]:
+        score_all = 5
+    elif threshold[2][2] < gm_all <= threshold[2][3]:
+        score_all = 5
+    else:
+        score_all = 0
+    return score_3m, score_6m, score_all
+
+def calculate_interest_rate(score, full_score):
+    score_ratio = score / full_score
+    placeholder = (1 + 0.5 * (1 - score_ratio)) * 0.015
+    rate = placeholder * 12
+    return round(placeholder, 4), round(rate, 4)
