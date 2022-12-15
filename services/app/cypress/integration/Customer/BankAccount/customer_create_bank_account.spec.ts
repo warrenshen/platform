fae3bb616569a0fd6734252863dbff12fb47d9cf
alan-bespoke <109971195+alan-bespoke@users.noreply.github.com>
2022-12-15 15:03:04 -0600

    init (#3106)

--- services/app/cypress/integration/Customer/BankAccount/customer_create_bank_account.spec.ts
+++ services/app/cypress/integration/Customer/BankAccount/customer_create_bank_account.spec.ts
@@ -29,6 +29,34 @@ describe("Bank Account Creation", () => {
       // Go to Customer > Settings
       cy.dataCy("sidebar-item-settings").click();
       cy.url().should("include", "settings");
+
+      // Open add bank account modal
+      cy.dataCy("add-bank-account-button").click();
+
+      // Fill out form - general bank info
+      cy.dataCySelector("bank-account-form-bank-name", "input").type(
+        "Cypress Bank"
+      );
+      cy.dataCySelector("bank-account-form-bank-account-name", "input").type(
+        "Cypress Checking"
+      );
+      cy.dataCy("bank-account-type-dropdown").click();
+      cy.dataCy("bank-account-type-dropdown-item").eq(0).click();
+      cy.dataCySelector("bank-account-form-account-number", "input").type(
+        "0023867512"
+      );
+
+      cy.dataCy("bank-account-form-ach-checkbox-container").click();
+      cy.dataCySelector("bank-account-form-ach-routing-number", "input").type(
+        "66039542"
+      );
+      cy.dataCySelector("bank-account-form-ach-default-memo", "input").type(
+        "Default ACH Memo"
+      );
+
+      // Submit and check for success alert
+      cy.dataCy("create-update-bank-account-modal-add-button").click();
+      cy.get(".MuiAlert-standardSuccess").should("exist");
     }
   );
 });
